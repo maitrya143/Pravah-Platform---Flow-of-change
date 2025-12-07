fd779a7c597840d6c8f89702335ed4608f4d2251
Maitrya Sontakke <85071123+maitrya143@users.noreply.github.com>
2025-12-07 14:51:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Pravah-Platform---Flow-of-change/",
   server: {
     port: 3000,
   }
